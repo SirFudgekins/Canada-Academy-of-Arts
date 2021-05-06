9b04856a23f327315e9b90bf822a839a1b88ac41
Glenn Ye <glenn.ye38@gmail.com>
2021-05-06 00:57:31 -0400

    Add theme and nav bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,18 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-// import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
+function IndexPage() {
+  return (
+    <Layout>
       <SEO title="Home" />
       <h1>This is the home page.</h1>
       <p>This is a sentence.</p>
-      {/*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>*/}
-      {/*    <Image />*/}
-      {/*</div>*/}
-
-      <Link to="/gallery/">Go to gallery</Link>
-  </Layout>
-)
+      <Link to="/gallery">Go to gallery</Link>
+    </Layout>
+  );
+}
 
 export default IndexPage

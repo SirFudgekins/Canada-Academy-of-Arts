a76fa453b35f7cb398f2b81b6c0da7db9842edaa
Glenn Ye <glenn.ye38@gmail.com>
2021-05-06 18:51:44 -0400

    Add footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,6 +13,7 @@ import Header from "./header";
 import "./layout.css";
 import { ThemeContext } from "./ThemeContext";
 import styled from "@emotion/styled";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -55,18 +56,14 @@ const Layout = ({ children }) => {
           <div
             style={{
               margin: `0 auto`,
-              maxWidth: 960,
+              maxWidth: `80vw`,
               padding: `0px 1.0875rem 1.45rem`,
               paddingTop: 0,
             }}
           >
             <main>{children}</main>
-            <footer>
-              © {new Date().getFullYear()}, Built with
-              {` `}
-              <a href="https://www.gatsbyjs.org">Gatsby</a>
-            </footer>
           </div>
+          <Footer />
         </ThemedLayout>
       )}
     </ThemeContext.Consumer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ function IndexPage() {
       <SEO title="Home" />
       <h1>This is the home page.</h1>
       <p>This is a sentence.</p>
-      <Link to="/gallery">Go to gallery</Link>
     </Layout>
   );
 }
